586961f62efc428ff39921b38a4e98eb217ed0c6
gyh214 <gyh214@gmail.com>
2026-01-13 01:01:31 +0900

    Remove final temporary script

---
+++ final_cleanup.bat
@@ -0,0 +1,4 @@
+del finalize_clean.bat
+git add -A
+git commit -m "Remove final temporary script"
+git push
--- finalize_clean.bat
+++
@@ -1,7 +0,0 @@
-del clean_temp.bat
-del git_check.bat
-del git_commit.bat
-del git_push.bat
-git add -A
-git commit -m "Remove temporary batch scripts"
-git push
